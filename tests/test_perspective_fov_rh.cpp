4946d7ef11f9850dbef2af4a6cee0f289256343e
LambdaXymox <lambda.xymox@gmail.com>
2024-06-08 17:16:00 -0500

    Add right-handed perspective fov tests.

---
+++ tests/test_perspective_fov_rh.cpp
@@ -0,0 +1,135 @@
+#include <gtest/gtest.h>
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm_metal/glm_metal.h>
+
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMatrix) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto expected = glm::mat4x4 {
+        3.0f / 4.0f, 0.0f,  0.0f,            0.0f,
+        0.0f,        1.0f,  0.0f,            0.0f,
+        0.0f,        0.0f, -100.0f / 99.0f, -1.0f,
+        0.0f,        0.0f, -100.0f / 99.0f,  0.0f
+    };
+    auto result = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+
+    EXPECT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsLeftToNegativeOneInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    float left = -aspectRatio * near;
+    auto vector = glm::vec4 { left, 0.0f, -2.0f, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = -1.0f;
+    float result = projectedVector.x;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsRightToPositiveOneInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    float right = aspectRatio * near;
+    auto vector = glm::vec4 { right, 0.0f, -2.0f, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = 1.0f;
+    float result = projectedVector.x;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsBottomToNegativeOneInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    float bottom = -near;
+    auto vector = glm::vec4 { 0.0f, bottom, -2.0f, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = -1.0f;
+    float result = projectedVector.y;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsTopToPositiveOneInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    float top = near;
+    auto vector = glm::vec4 { 0.0f, top, -2.0f, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = 1.0f;
+    float result = projectedVector.y;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsPositiveNearToZeroInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    auto vector = glm::vec4 { 0.0f, 0.0f, -near, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = 0.0f;
+    float result = projectedVector.z;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionMapsPositiveFarToPositiveOneInNDC) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    auto vector = glm::vec4 { 0.0f, 0.0f, -far, 1.0f };
+    auto projectedVector = matrix * vector;
+    float expected = 100.0f;
+    float result = projectedVector.z;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionPreservesDepthOrdering) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    auto point1 = matrix * glm::vec4 { 0.0f, 0.0f, -2.0f, 1.0f };
+    auto point2 = matrix * glm::vec4 { 0.0f, 0.0f, -99.0f, 1.0f};
+
+    EXPECT_LE(point1.z, point2.z);
+}
+
+TEST(PerspectiveFovRhTests, PerspectiveProjectionHomogeneousCoordinate) {
+    float fovyRadians = glm::pi<float>() / 2.0f;
+    float aspectRatio = 4.0f / 3.0f;
+    float near = 1.0f;
+    float far = 100.0f;
+    auto matrix = glm_metal::perspective_fov_rh(fovyRadians, aspectRatio, near, far);
+    auto point = glm::vec4 { 0.0f, 0.0f, -99.0f, 1.0f };
+    auto projectedPoint = matrix * point;
+    float expected = 99.0f;
+    float result = projectedPoint.w;
+
+    EXPECT_FLOAT_EQ(result, expected);
+}
